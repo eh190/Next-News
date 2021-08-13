d505eec876076950c161d7b5092f97a0ab4b5cf7
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2021-08-13 16:56:06 +0100

    Added business page data

--- pages/business.js
+++ pages/business.js
@@ -1,8 +1,33 @@
-const Business = () => {
+import Content from "../components/Content/Content";
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://newsapi.org/v2/top-headlines?country=gb&category=business&pageSize=10",
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
+      },
+    }
+  );
+
+  const data = await res.json();
+  console.log(data);
+
+  return {
+    props: {
+      data: data,
+    },
+    //Refreshes the fetch every hour to rebuild page with new stories
+    revalidate: 60 * 60,
+  };
+};
+
+const Business = ({ data }) => {
   return (
     <main className="main">
       <h1 className="pageTitle">Business NEWS</h1>
-      <div className="container"></div>
+      <hr className="line" />
+      <Content data={data} />
     </main>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Content from "../components/Content/Content";
 import styles from "../styles/Home.module.scss";
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const res = await fetch("https://newsapi.org/v2/top-headlines?country=gb", {
     headers: {
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
@@ -16,6 +16,8 @@ export const getServerSideProps = async () => {
     props: {
       data: data,
     },
+    //Refreshes the fetch every hour to rebuild page with new stories
+    revalidate: 60 * 60,
   };
 };
 
