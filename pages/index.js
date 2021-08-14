5870c6c4037927c2c31083bea767068f16e5842b
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2021-08-14 13:33:31 +0100

    removed unused variable

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import Content from "../components/Content/Content";
-import styles from "../styles/Home.module.scss";
 
 export default function Home({ data }) {
   return (
